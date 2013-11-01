0bbd0d2535525d02d9378949a16ad30845470c4f
Alan Spencer <a.spencer09@imperial.ac.uk>
2013-11-01 12:18:12 +0000

    Added GPLv3 to all header and source files

--- matrixsettingsdialog.cpp
+++ matrixsettingsdialog.cpp
@@ -1,3 +1,20 @@
+/*------------------------------------------------------------------------------------------------------
+ * Matrix Data Editor (MaDE)
+ *
+ * Copyright (c) 2012-2013, Alan R.T. Spencer
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not,
+ * see http://www.gnu.org/licenses/.
+ *-----------------------------------------------------------------------------------------------------*/
+
 #include "matrixsettingsdialog.h"
 
 MatrixSettingsDialog::MatrixSettingsDialog(QWidget *parent) :
--- nexusreadertoken.h
+++ nexusreadertoken.h
@@ -1,4 +1,21 @@
 /*------------------------------------------------------------------------------------------------------
+ * Matrix Data Editor (MaDE)
+ *
+ * Copyright (c) 2012-2013, Alan R.T. Spencer
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not,
+ * see http://www.gnu.org/licenses/.
+ *-----------------------------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------------------------------
  * Modified from work by Paul O. Lewis and Mark T. Holder. Available from http://sourceforge.net/p/ncl/
  * under a GNU General Public License version 2.0 (GPLv2) Licence.
  *
